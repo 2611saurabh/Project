e46187bd76b9433b79dfb7b20d635dd1139e08b5
2611saurabh <saurabh.kr026@gmail.com>
2023-08-15 10:54:13 +0530

    first commit

---
+++ Frontend/index.js
@@ -0,0 +1,58 @@
+//word vs keyword
+
+
+//var vs const
+
+var  dulha = "Lab";
+const dulhan = "Labi";
+
+//hoisting -> we use the variable and function  before declartion
+
+console.log(a);
+
+var a  =12;
+
+
+// difference between undefined and not-defined
+//undefined -> when any things have their exitence but not having any specific  value
+// var a;  (here we have declared a now a has existence but there is no value so its undefined)
+
+//not-defined : whan we call any thing but it not having exixetence
+//such that we print console.log(b); but we never declared b or intialize b 
+//
+
+
+//types in js
+
+//-> primitive and refrences
+
+/*
+primitives = number, string, null, undefined, boolean
+refrences = [] {} ()
+ aisi koi  bhi value jisko copy karne per real copy nahi hoat hai, 
+balki us main value ka refrence pass ho jata hai,
+use hum refrences value kahte hai, aur jiska copy karne par real copy ho
+jayewo primitive value hoti hai
+
+*/
+
+/*
+
+var a = [1,2,3,4];
+var b = a;
+
+b.pop();
+console.log(b);
+
+console.log(a);
+
+*/
+
+
+// how refrence work;
+
+
+
+
+
+
